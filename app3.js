25392bdd3b255ce4c86a3df66c2b00b979757d11
Carlos Verdugo <carlosverdugo@proton.me>
2024-08-25 19:36:36 -0700

    Counting react words

--- app3.js
+++ app3.js
@@ -1,3 +1,19 @@
 const fs = require('fs');
 
 const content = fs.readFileSync('README.md', 'utf8');
+
+const words = content.split(' ');
+
+// console.log('Palabras: ', wordCount);
+
+// let reactWordCount = 0;
+
+// words.map((item) => {
+//     if (item.toLowerCase() === 'react') reactWordCount++
+// });
+
+// const reactWordCount = words.filter( word => word.toLowerCase().includes('react')).length
+
+const reactWordCount = content.match(/react/gi ?? []).length;
+
+console.log('Palabras React:', reactWordCount)
